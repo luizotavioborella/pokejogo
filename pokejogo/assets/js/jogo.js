6bc77a48d613bb283ead00dffda6f902123a3aeb
Luiz Otávio Borella e Souza <luiz.o.souza6@edu.senai.br>
2026-09-11 09:29:02 -0300

    Add files via upload

--- pokejogo/assets/js/jogo.js
+++ pokejogo/assets/js/jogo.js
@@ -2,7 +2,7 @@ var velocidade = 1
 var jogoIniciado = false
 var record = localStorage.getItem("record")
 
-if(record === null){
+if (record === null) {
     record = 0
 }
 
@@ -12,17 +12,21 @@ const musica = document.querySelector("#musica")
 const somDano = document.querySelector("#somDano")
 const somPulo = document.querySelector("#somPulo")
 
+musica.volume = 1
+somDano.volume = 0.5
+somPulo.volume = 0.5
+
 const cenario = {
     left: 0,
     jogo: document.querySelector("#jogo"),
 
-    movimentar(){
+    movimentar() {
         this.left = this.left - velocidade
         this.jogo.style.backgroundPositionX = this.left + "px"
     },
 
-    verificarGameOver(){
-        if(personagem.vidas <= 0){
+    verificarGameOver() {
+        if (personagem.vidas <= 0) {
             jogoIniciado = false
             musica.pause()
             document.querySelector("#pontosGameOver").innerHTML = personagem.pontos
@@ -35,52 +39,52 @@ const vilao = {
     right: -153,
     item: document.querySelector("#vilao"),
 
-    movimentar(){
+    movimentar() {
         this.right = this.right + (velocidade * 1.7)
         this.item.style.right = this.right + "px"
 
         var larguraJogo = cenario.jogo.offsetWidth
 
-        if(larguraJogo < this.right){
+        if (larguraJogo < this.right) {
             this.right = -153
             personagem.pontos = personagem.pontos + 10
 
-            if(personagem.pontos === 80){
+            if (personagem.pontos === 80) {
                 this.item.style.backgroundImage = "url('assets/img/vilao2.png')"
             }
 
-            if(personagem.pontos === 100){
+            if (personagem.pontos === 100) {
                 cenario.jogo.style.backgroundImage = "url('assets/img/cenario2.png')"
             }
 
-            if(personagem.pontos === 150){
+            if (personagem.pontos === 150) {
                 personagem.limitePulos = 2
             }
 
-            if(personagem.pontos === 160){
+            if (personagem.pontos === 160) {
                 this.item.style.backgroundImage = "url('assets/img/vilao3.png')"
             }
 
-            if(personagem.pontos === 200){
+            if (personagem.pontos === 200) {
                 cenario.jogo.style.backgroundImage = "url('assets/img/cenario3.png')"
                 personagem.item.style.backgroundImage = "url('assets/img/jogador2.png')"
             }
 
-            if(personagem.pontos === 240){
+            if (personagem.pontos === 240) {
                 this.item.style.backgroundImage = "url('assets/img/vilao4.png')"
             }
 
-            if(personagem.pontos === 300){
+            if (personagem.pontos === 300) {
                 cenario.jogo.style.backgroundImage = "url('assets/img/cenario4.png')"
             }
 
-            if(personagem.pontos > record){
+            if (personagem.pontos > record) {
                 record = personagem.pontos
                 localStorage.setItem("record", record)
                 document.querySelector("#record").innerHTML = record
             }
 
-            if(personagem.pontos % 50 === 0 && velocidade < 4){
+            if (personagem.pontos % 50 === 0 && velocidade < 4) {
                 velocidade = velocidade + 0.5
             }
 
@@ -88,31 +92,31 @@ const vilao = {
         }
     },
 
-    colisao(){
+    colisao() {
         var posicaoJogador = personagem.item.getBoundingClientRect()
         var posicaoVilao = this.item.getBoundingClientRect()
 
-        if(
+        if (
             posicaoJogador.left < posicaoVilao.left &&
             posicaoJogador.right > posicaoVilao.left &&
             posicaoJogador.bottom > posicaoVilao.top &&
             posicaoJogador.top <= posicaoVilao.top
-        ){
+        ) {
             personagem.vidas = personagem.vidas - 1
 
             somDano.pause()
             somDano.currentTime = 0.71
             somDano.play()
 
-            if(personagem.vidas === 2){
+            if (personagem.vidas === 2) {
                 document.querySelector("#vida3").style.display = "none"
             }
 
-            if(personagem.vidas === 1){
+            if (personagem.vidas === 1) {
                 document.querySelector("#vida2").style.display = "none"
             }
 
-            if(personagem.vidas === 0){
+            if (personagem.vidas === 0) {
                 document.querySelector("#vida1").style.display = "none"
             }
 
@@ -144,33 +148,53 @@ const personagem = {
     velocidadeMovimento: 3,
     item: document.querySelector("#personagem"),
 
-    movimentar(){
-        if(this.esquerda === true && this.left > 0){
+    movimentar() {
+        if (this.esquerda === true && this.left > 0) {
             this.left = this.left - this.velocidadeMovimento
         }
 
-        if(this.direita === true && this.left < cenario.jogo.offsetWidth - 153){
+        if (this.direita === true && this.left < cenario.jogo.offsetWidth - 153) {
             this.left = this.left + this.velocidadeMovimento
         }
 
         this.item.style.left = this.left + "px"
 
-        if(this.bottom > 70 || this.velocidadeY > 0){
+        if (this.bottom > 70 || this.velocidadeY > 0) {
             this.bottom = this.bottom + this.velocidadeY
             this.velocidadeY = this.velocidadeY - this.gravidade
         }
 
-        if(this.bottom <= 70){
+        if (this.bottom <= 70) {
             this.bottom = 70
             this.velocidadeY = 0
             this.pulos = 0
         }
 
         this.item.style.bottom = this.bottom + "px"
+        this.animar()
     },
 
-    pular(){
-        if(this.pulos < this.limitePulos){
+    animar() {
+        if (this.bottom > 70) {
+            this.item.style.animation = "none"
+
+            if (this.velocidadeY > 6) {
+                this.item.style.backgroundPosition = "0px -245px"
+            } else if (this.velocidadeY > 2) {
+                this.item.style.backgroundPosition = "-151px -219px"
+            } else if (this.velocidadeY > -2) {
+                this.item.style.backgroundPosition = "-305px -214px"
+            } else {
+                this.item.style.backgroundPosition = "-453px -231px"
+            }
+        } else {
+            this.item.style.animation = "moveJogador 0.4s infinite step-end"
+            this.item.style.backgroundPositionY = "-64px"
+        }
+    },
+
+    pular() {
+        if (this.pulos < this.limitePulos) {
             this.velocidadeY = this.forcaPulo
             this.pulos = this.pulos + 1
             return true
@@ -180,8 +204,8 @@ const personagem = {
     }
 }
 
-setInterval(function(){
-    if(jogoIniciado === true){
+setInterval(function () {
+    if (jogoIniciado === true) {
         cenario.movimentar()
         vilao.movimentar()
         personagem.movimentar()
@@ -190,18 +214,18 @@ setInterval(function(){
     }
 }, 10)
 
-document.addEventListener("keydown", function(event){
-    if(jogoIniciado === true){
-        if(event.code === "ArrowLeft"){
+document.addEventListener("keydown", function (event) {
+    if (jogoIniciado === true) {
+        if (event.code === "ArrowLeft") {
             personagem.esquerda = true
         }
 
-        if(event.code === "ArrowRight"){
+        if (event.code === "ArrowRight") {
             personagem.direita = true
         }
 
-        if(event.code === "Space" && event.repeat === false){
-            if(personagem.pular() === true){
+        if (event.code === "Space" && event.repeat === false) {
+            if (personagem.pular() === true) {
                 somPulo.pause()
                 somPulo.currentTime = 0.56
                 somPulo.play()
@@ -210,17 +234,17 @@ document.addEventListener("keydown", function(event){
     }
 })
 
-document.addEventListener("keyup", function(event){
-    if(event.code === "ArrowLeft"){
+document.addEventListener("keyup", function (event) {
+    if (event.code === "ArrowLeft") {
         personagem.esquerda = false
     }
 
-    if(event.code === "ArrowRight"){
+    if (event.code === "ArrowRight") {
         personagem.direita = false
     }
 })
 
-document.querySelector("#btnComecar").addEventListener("click", function(){
+document.querySelector("#btnComecar").addEventListener("click", function () {
     jogoIniciado = true
     document.querySelector("#telaInicio").style.display = "none"
     personagem.item.style.visibility = "visible"
@@ -230,7 +254,7 @@ document.querySelector("#btnComecar").addEventListener("click", function(){
     musica.play()
 })
 
-document.querySelector("#btnReiniciar").addEventListener("click", function(){
+document.querySelector("#btnReiniciar").addEventListener("click", function () {
     personagem.pontos = 0
     personagem.vidas = 3
     personagem.left = 200
@@ -243,6 +267,8 @@ document.querySelector("#btnReiniciar").addEventListener("click", function(){
     personagem.item.style.left = "200px"
     personagem.item.style.bottom = "70px"
     personagem.item.style.backgroundImage = "url('assets/img/jogador.png')"
+    personagem.item.style.animation = "moveJogador 0.4s infinite step-end"
+    personagem.item.style.backgroundPositionY = "-64px"
 
     vilao.right = -153
     vilao.item.style.right = "-153px"
